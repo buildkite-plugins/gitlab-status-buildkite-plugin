f0d4a2795f7bbdac200de393519d8ba9f4b68ae9
Matías A. Bellone <matias.bellone@buildkite.com>
2022-12-30 19:48:19 -0300

    Corrected encoding of project

---
+++ lib/urls.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+# Licensed under CC-BY-SA 4.0
+# Source: https://stackoverflow.com/a/10660730
+# Almost verbatim
+urlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"
+}
